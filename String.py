16d94f2a36b76cda416758efab0c7b477993a8c1
Abhishek kumar <116196351+abhishekkumar956821@users.noreply.github.com>
2024-10-15 10:06:07 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,36 @@
+name = "Abhishek kumar"
+friend = "Deepanshu"
+anotherfriend = 'lavi'
+apple = ''' he said
+hi Abhishek 
+hey i am good 
+i want to eat an apple'''
+print("hello," + name)
+print(apple)
+# In python , String is like an array of characters.
+#  we can access parts of string by using its index which start from 0.
+#Square brackets can be used to access element of the string.
+
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+
+
+print("\n")
+
+
+print(friend[0])
+print(friend[1])
+print(friend[2])
+print(friend[3])
+print(friend[4])
+print(friend[5])
+print(friend[6])
+print(friend[7])
+print(friend[8])
